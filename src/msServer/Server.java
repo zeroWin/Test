7ecbc515781c5f3fbae1f8836c3d14a19559317c
zeroWin <2541497180@qq.com>
2016-05-15 12:30:26 +0000

    Test1 local file read version 1 not OK

--- src/msServer/Server.java
+++ src/msServer/Server.java
@@ -1,5 +1,6 @@
 package msServer;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -36,6 +37,10 @@ public class Server {
 		solutionIdToAuId = new SolutionIdToAuId();
 		
 		apiuse = new APIuse();
+		apiuse.setCount("50000"); // 个数
+		apiuse.setAttributes("Id,C.CId,F.FId,J.JId,AA.AuId,AA.AfId,RId"); // 返回的东西，根据路径分析图，id1所有参数都需要
+		apiuse.setOffset("0"); // 偏移
+		
 		HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
 		server.createContext("/Bupt", new MyHandler());
 		server.setExecutor(null); // creates a default executor
@@ -47,7 +52,7 @@ public class Server {
 		public void handle(HttpExchange httpExchange) throws IOException {
 			long st = System.nanoTime();
 			// 解析url并获取id1和id2
-//			long st1 = System.nanoTime();
+			long st1 = System.nanoTime();
 			String response = null;
 			String strUrlParam = new String(httpExchange.getRequestURI().getQuery());
 			System.out.println("输入为："+strUrlParam);
@@ -56,6 +61,8 @@ public class Server {
 			String[] arrSplitId2 = arrSplit[1].split("=");
 			String id1 = null;
 			String id2 = null;
+			List<Entities> EntitiesId1 = null;
+			List<Entities> EntitiesId2 = null;
 			if(arrSplitId1[0].equals("id1"))
 			{
 				id1 = arrSplitId1[1];
@@ -68,57 +75,93 @@ public class Server {
 			}
 //			System.out.println("字符串处理所用时间： "+(System.nanoTime()-st1));
 			// 获取并显示id1和id2的类型
-//			st1 = System.nanoTime();
+			st1 = System.nanoTime();
 			IDtype id1Type,id2Type;
 			
-			apiuse.setCount("50000"); // 个数
-			apiuse.setAttributes("Id,C.CId,F.FId,J.JId,AA.AuId,AA.AfId,RId"); // 返回的东西，根据路径分析图，id1所有参数都需要
-			apiuse.setOffset("0"); // 偏移
+			File fileReadId1 = new File("D:\\Id\\Id="+id1+".txt");
+			if(fileReadId1.exists()) // 判断Id1
+			{
+				System.out.println("Id1文件存在类型为Id");
+				id1Type = IDtype.ID;
+				// 读取文件
+			}
+			else
+			{
+				fileReadId1 = new File("D:\\AuId\\Composite(AA.AuId="+id1+").txt");
+				if(fileReadId1.exists())
+				{
+					System.out.println("Id1文件存在类型为AuId");
+					id1Type = IDtype.AA_AuId;
+					// 读取文件
+				}
+				else //搜索
+				{
+					System.out.println("Id1文件不存在搜索中");
+					String exprId1 = new StringBuilder(27+id1.length()+id1.length())
+							.append("Or(Composite(AA.AuId=").append(id1).append("),Id=").append(id1).append(")").toString();
+					apiuse.setExpr(exprId1);
+					ResultJsonClass searchResultId = apiuse.HandleURI(apiuse.getURI());
+					EntitiesId1 = searchResultId.entities;
+			        if(EntitiesId1.size() > 1) // 实体个数大于1肯定是AuId
+			        	id1Type = IDtype.AA_AuId;
+			        else // 实体个数为1，那就判断实体的Id与id1相等吗
+			        {
+			        	if(EntitiesId1.get(0).Id.equals(id1)) //相等，id1是论文id
+			        		id1Type = IDtype.ID;
+			        	else
+			        		id1Type = IDtype.AA_AuId;
+			        }
+				}
+			}
 			
-			String exprId1 = new StringBuilder(27+id1.length()+id1.length())
-								.append("Or(Composite(AA.AuId=").append(id1).append("),Id=").append(id1).append(")").toString();
-			apiuse.setExpr(exprId1);
-	        ResultJsonClass searchResultId = apiuse.HandleURI(apiuse.getURI());
-	        List<Entities> EntitiesId1 = searchResultId.entities;
-			
-			String exprId2 = new StringBuilder(27+id2.length()+id2.length())
-			.append("Or(Composite(AA.AuId=").append(id2).append("),Id=").append(id2).append(")").toString();
-			apiuse.setExpr(exprId2);
-			apiuse.setAttributes("Id,C.CId,F.FId,J.JId,AA.AuId,AA.AfId"); // 返回的东西，根据路径分析图，不需要id2的RId
-			searchResultId = apiuse.HandleURI(apiuse.getURI());
-	        List<Entities> EntitiesId2 = searchResultId.entities;
+			File fileReadId2 = new File("D:\\Id\\Id="+id2+".txt");
+			if(fileReadId2.exists()) // 判断Id1
+			{
+				System.out.println("Id1文件存在类型为Id");
+				id2Type = IDtype.ID;
+				// 读取文件
+			}
+			else
+			{
+				fileReadId2 = new File("D:\\AuId\\Composite(AA.AuId="+id2+").txt");
+				if(fileReadId2.exists())
+				{
+					System.out.println("Id1文件存在类型为AuId");
+					id2Type = IDtype.AA_AuId;
+					// 读取文件
+				}
+				else //搜索
+				{
+					System.out.println("文件不存在搜索中");		
+					String exprId2 = new StringBuilder(27+id2.length()+id2.length())
+									.append("Or(Composite(AA.AuId=").append(id2).append("),Id=").append(id2).append(")").toString();
+					apiuse.setExpr(exprId2);
+					apiuse.setAttributes("Id,C.CId,F.FId,J.JId,AA.AuId,AA.AfId"); // 返回的东西，根据路径分析图，不需要id2的RId
+					ResultJsonClass searchResultId  = apiuse.HandleURI(apiuse.getURI());
+					EntitiesId2 = searchResultId.entities;
+			        if(EntitiesId2.size() > 1) // 实体个数大于1肯定是AuId
+			        	id2Type = IDtype.AA_AuId;
+			        else // 实体个数为1，那就判断实体的Id与id1相等吗
+			        {
+			        	if(EntitiesId2.get(0).Id.equals(id2)) //相等，id2是论文id
+			        		id2Type = IDtype.ID;
+			        	else
+			        		id2Type = IDtype.AA_AuId;
+			        }	
+				}
+			}			
+
+	        if(id1Type == IDtype.ID)
+	        	System.out.println("id1="+id1+"类型为Id");
+	        else
+	        	System.out.println("id1="+id1+"类型为AuId");
 	        
-	        if(EntitiesId1.size() > 1) // 实体个数大于1肯定是AuId
-	        	id1Type = IDtype.AA_AuId;
-	        else // 实体个数为1，那就判断实体的Id与id1相等吗
-	        {
-	        	if(EntitiesId1.get(0).Id.equals(id1)) //相等，id1是论文id
-	        		id1Type = IDtype.ID;
-	        	else
-	        		id1Type = IDtype.AA_AuId;
-	        }
+	        if(id2Type == IDtype.ID)
+	        	System.out.println("id2="+id2+"类型为Id");
+	        else
+	        	System.out.println("id2="+id2+"类型为AuId");
 	        
-	        if(EntitiesId2.size() > 1) // 实体个数大于1肯定是AuId
-	        	id2Type = IDtype.AA_AuId;
-	        else // 实体个数为1，那就判断实体的Id与id1相等吗
-	        {
-	        	if(EntitiesId2.get(0).Id.equals(id2)) //相等，id2是论文id
-	        		id2Type = IDtype.ID;
-	        	else
-	        		id2Type = IDtype.AA_AuId;
-	        }	        
-	        
-//	        if(id1Type == IDtype.ID)
-//	        	System.out.println("id1="+id1+"类型为Id");
-//	        else
-//	        	System.out.println("id1="+id1+"类型为AuId");
-//	        
-//	        if(id2Type == IDtype.ID)
-//	        	System.out.println("id2="+id2+"类型为Id");
-//	        else
-//	        	System.out.println("id2="+id2+"类型为AuId");
-	        
-//			System.out.println("类型判断使用时间： "+(System.nanoTime()-st1));
+			System.out.println("类型判断+第一次搜索使用时间： "+(System.nanoTime()-st1));
 			// 处理算法
 
 			if(id1Type == id2Type)
@@ -153,7 +196,7 @@ public class Server {
 //					response = "[]";
 					String result = solutionAuIdToId.AuIdToId_All(id1, id2,EntitiesId1,EntitiesId2.get(0));
 					response =  new StringBuilder(3+result.length()).append("[")
-							.append(result).append("]").toString();;
+							.append(result).append("]").toString();
 				}
 				
 			}
